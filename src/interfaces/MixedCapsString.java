be138d6ba1ffe443200bf18bda0011449b3ead35
J.D.S. Solutions <League@AdministorsiMac.attlocal.net>
2018-02-02 17:35:18 -0800

    Done with module 3

--- src/interfaces/MixedCapsString.java
+++ src/interfaces/MixedCapsString.java
@@ -11,13 +11,27 @@ public class MixedCapsString extends SpecialString {
 	public String funkifyText(String s) {
 		// TODO Auto-generated method stub
 		char[] chars = s.toCharArray();
-		for (int i = 0; i <= chars.length - 1; i++) {
-			if (i % 2 == 1) {
-
+		String temp = "";
+		String r = "";
+		for (int i = 0; i < chars.length; i ++) {
+			if(i == 0) {
+				temp = chars[i] + "";
+				temp = temp.toLowerCase();
+				chars[i] = temp.charAt(0);
+				temp = "";
+			}else if(i % 2 == 1) {
+				temp = chars[i] + "";
+				temp = temp.toUpperCase();
+				chars[i] = temp.charAt(0);
+				temp = "";
 			}
+
 		}
-		return s;
+		for (int i = 0; i < chars.length; i++) {
+			r += chars[i];
+		}
+		System.out.println(r);
+		return r;
 	}
-	// fail
 
 }
